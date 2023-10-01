f4e8646074526b223e30422ff2f16a772d4ff5a6
abdurrahim220 <abdurrahim5@gmail.com>
2023-10-01 09:59:06 +0600

    object

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import User from './components/User';
+import "./App.css";
+import User from "./components/User";
+
+const user1 = {
+  name: "md rifat uddin",
+  age: 24,
+  isRegistered: false,
+  lang: ["bangla", "english"],
+};
+const user2 = {
+  name: "md rafik uddin",
+  age: 24,
+  isRegistered: false,
+  lang: ["bangla", "english"],
+};
 
 function App() {
   return (
     <div className="App">
-      
       <h1>User Management</h1>
-      <User name="md ab rahim" age={24}/>
+      {/* <User
+        name="md ab rahim"
+        age={24}
+        isRegistered={true}
+        lang={["bangla", "english"]}
+      />
+      <User
+        name="md rifat uddin"
+        age={24}
+        isRegistered={false}
+        lang={["bangla", "english"]}
+      /> */}
+      <User user={user1} />
+      <User user={user2} />
     </div>
   );
 }
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,20 +1,41 @@
-import React from 'react'
+import React from "react";
 
+//? string ,number ,boolean ,void , null
+//? user defined types -> object,array,enum,union,any,custom type
 
-// string ,number ,boolean ,void , null
+type userProps = {
+  // name: string;
+  // age: number;
+  // isRegistered: boolean;
+  // lang: string[];
+  user: {
+    name: string;
+    age: number;
+    isRegistered: boolean;
+    lang: string[];
+  };
+};
 
-type userProps =  {name:string , age:number}
+//!User = (props : userProps)
+//!User = ({ name, age, isRegistered, lang }: userProps)
 
-const User = (props : userProps) => {
+const User = ({ user }: userProps) => {
   return (
-    <div>
-        {/* <h1>Md Abdur Rahim</h1>
+    <div style={{border:'2px solid',margin:'1rem'}}>
+      {/* <h1>Md Abdur Rahim</h1>
         <p>z.abdurrahim5@gmail.com</p>
         <p>24 years old</p> */}
-        <h1>{props.name}</h1>
-        <p>{props.age} years old</p>
+      <h1>{user.name}</h1>
+      <p>{user.age} years old</p>
+      {user.isRegistered ? <p>Registered User</p> : <p>Not registered User</p>}
+      <p>
+        Speaks:{" "}
+        {user.lang.map((language, index) => {
+          return <span key={index}>{language} </span>;
+        })}
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default User
+export default User;
